c206a4ba3bda68fed0905b809209326d789f9ba0
Daniel Hollands <daniel@limeblast.co.uk>
2020-07-28 20:25:19 +0100

    ¯\_(ツ)_/¯

--- src/js/cms.js
+++ src/js/cms.js
@@ -6,14 +6,14 @@ import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../c
 
 import HomePreview from "./cms-preview-templates/home";
 import BlogPreview from "./cms-preview-templates/blog";
-import ProductsPreview from "./cms-preview-templates/products";
+import ServicesPreview from "./cms-preview-templates/services";
 import ValuesPreview from "./cms-preview-templates/values";
 import ContactPreview from "./cms-preview-templates/contact";
 
-CMS.registerPreviewStyle(styles, { raw: true });
+CMS.registerPreviewStyle(styles, {raw: true});
 CMS.registerPreviewTemplate("home", HomePreview);
 CMS.registerPreviewTemplate("blog", BlogPreview);
-CMS.registerPreviewTemplate("products", ProductsPreview);
+CMS.registerPreviewTemplate("services", ServicesPreview);
 CMS.registerPreviewTemplate("values", ValuesPreview);
 CMS.registerPreviewTemplate("contact", ContactPreview);
 CMS.init();
